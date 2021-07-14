6689d6e272c0c72685c07223cb2dbaf2e18ce525
Miles Thompson <utunga@gmail.com>
2021-07-14 12:29:24 +1200

    fix bug for css
    Added NOTE2DH in two places also

--- src/components/Governance.js
+++ src/components/Governance.js
@@ -7,83 +7,74 @@ import { Card, Container, Row, Col } from "reactstrap";
 
 function Governance() {
   return (
-    <>
-      <div className="cd-section" id="features">
-        <div
-          className="govern section-image"
-          style={{
-            backgroundImage: "url(" + require("assets/img/bg11.jpg") + ")",
-          }}
-        >
-          <Col className="mr-auto ml-auto text-center" md="8">
-            <h2 className="title">Working is a pleasure</h2>
-            <h4 className="description">
-              This is the paragraph where you can write more details about your
-              product. Keep you user engaged by providing meaningful
-              information.
-            </h4>
-            <h2 className="title">Lead</h2>
-            <h4 className="description">
-              This is the paragraph where you can write more details about your
-              product. Keep you user engaged by providing meaningful
-              information.
-            </h4>
-          </Col>
-          <Container fluid>
-            <Row>
-              <Col className="px-0" md="6">
-                <Col sm="12">
-                  <div className="info info-horizontal">
-                    <div className="icon">
-                      <i className="now-ui-icons design-2_html5"></i>
-                    </div>
-                    <div className="description">
-                      <h5 className="info-title">For Developers</h5>
-                      <p className="description">
-                        The moment you use Now UI Kit, you know you’ve never
-                        felt anything like it. With a single use, this powerfull
-                        UI Kit lets you do more than ever before.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="info info-horizontal">
-                    <div className="icon">
-                      <i className="now-ui-icons design_palette"></i>
-                    </div>
-                    <div className="description">
-                      <h5 className="info-title">For Designers</h5>
-                      <p className="description">
-                        The moment you use Now UI Kit, you know you’ve never
-                        felt anything like it. With a single use, this powerfull
-                        UI Kit lets you do more than ever before.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="info info-horizontal">
-                    <div className="icon">
-                      <i className="now-ui-icons design-2_ruler-pencil"></i>
-                    </div>
-                    <div className="description">
-                      <h5 className="info-title">For Artists</h5>
-                      <p className="description">
-                        The moment you use Now UI Kit, you know you’ve never
-                        felt anything like it. With a single use, this powerfull
-                        UI Kit lets you do more than ever before.
-                      </p>
-                    </div>
-                  </div>
-                </Col>
-              </Col>
-              <Col md="6">
-                <div className="image-container">
-                  <img alt="..." src={require("assets/img/ipad3.png")}></img>
+    <div className="govern section-image"
+      style={{
+        backgroundImage: "url(" + require("assets/img/bg11.jpg") + ")",
+      }}>
+      <Col className="mr-auto ml-auto text-center" md="8">
+        <h2 className="title">About us</h2>
+        <h4 className="description">
+          This is the paragraph where you can write more details about your
+          product. Keep you user engaged by providing meaningful
+          information.
+        </h4>
+      </Col>
+     {/*NOTE2DH: 
+       It's weird that this is <Container fluid but the others 
+       are just <Container */}
+      <Container fluid>
+        <Row>
+          <Col className="px-0" md="6">
+            <Col sm="12">
+              <div className="info info-horizontal">
+                <div className="icon">
+                  <i className="now-ui-icons design-2_html5"></i>
                 </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </div>
-    </>
+                <div className="description">
+                  <h5 className="info-title">For Developers</h5>
+                  <p className="description">
+                    The moment you use Now UI Kit, you know you’ve never
+                    felt anything like it. With a single use, this powerfull
+                    UI Kit lets you do more than ever before.
+                  </p>
+                </div>
+              </div>
+              <div className="info info-horizontal">
+                <div className="icon">
+                  <i className="now-ui-icons design_palette"></i>
+                </div>
+                <div className="description">
+                  <h5 className="info-title">For Designers</h5>
+                  <p className="description">
+                    The moment you use Now UI Kit, you know you’ve never
+                    felt anything like it. With a single use, this powerfull
+                    UI Kit lets you do more than ever before.
+                  </p>
+                </div>
+              </div>
+              <div className="info info-horizontal">
+                <div className="icon">
+                  <i className="now-ui-icons design-2_ruler-pencil"></i>
+                </div>
+                <div className="description">
+                  <h5 className="info-title">For Artists</h5>
+                  <p className="description">
+                    The moment you use Now UI Kit, you know you’ve never
+                    felt anything like it. With a single use, this powerfull
+                    UI Kit lets you do more than ever before.
+                  </p>
+                </div>
+              </div>
+            </Col>
+          </Col>
+          <Col md="6">
+            <div className="image-container">
+              <img alt="..." src={require("assets/img/ipad3.png")}></img>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
