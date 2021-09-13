8791830a9a91bbbd12736aab1608d5c513a178b8
Karen <karen@laterolabs.com>
2021-09-13 03:54:17 -0700

    Change visuals (#8)
    * changing out some of the graphics/icons to better match
    
    * Resolving merge and adjusting new imagery

--- src/components/Details.js
+++ src/components/Details.js
@@ -11,8 +11,7 @@ function Details() {
   return (
     <>
       <div className="features-5">
-        
-        <div className="credit-lifecycle py-5 my-5">
+        <div className="credit-lifecycle bg-grey py-5 my-5">
           <Container>
             <h2 className="title">The Cashless Credit Cycle</h2>
             <Row className="no-gutters">
@@ -52,10 +51,14 @@ function Details() {
                     <i className="now-ui-icons arrows-1_refresh-69"></i>
                   </div>
                   <div className="description text-left">
-                    <h5 className="info-title font-weight-bold">Reciprocity</h5>
-                    <p>
-                      The simplest way to erase your cashless credits is offering back an equal value of your time in exchange. For example, if a graphic designer helped you design a logo and you helped with some of their work in turn you have found a ‘reciprocity’ loop that erases the debt. But you don't have to find an exact swap. The Cashless AI works hard at finding and suggesting trades that will help you erase your outstanding debits or spend your credits.
-                    </p>
+                    <Row>
+                      <Col>
+                      <h5 className="info-title font-weight-bold">Reciprocity</h5>
+                        <p>
+                          The simplest way to erase your cashless credits is offering back an equal value of your time in exchange. For example, if a graphic designer helped you design a logo and you helped with some of their work in turn you have found a ‘reciprocity’ loop that erases the debt. But you don't have to find an exact swap. The Cashless AI works hard at finding and suggesting trades that will help you erase your outstanding debits or spend your credits.
+                        </p>
+                      </Col>
+                    </Row>
                   </div>
                 </div>
               </Col>
@@ -68,10 +71,14 @@ function Details() {
                     <i className="now-ui-icons emoticons_satisfied"></i>
                   </div>
                   <div className="description text-left">
-                      <h5 className="info-title font-weight-bold">Vest & Spend</h5>
-                      <p>
-                        Like any living system, the reserves underpinning cashless promises undergo a natural birth, growth and decay cycle. If no reciprocity is found, the value of reserves build up over time, rewarding the recipient for holding on to the promise token. As they mature, promises bear fruit in the form of hard currency reserves, that can be picked into the receiver’s wallet as soon as they vest.
-                      </p>
+                    <Row>
+                      <Col>
+                        <h5 className="info-title font-weight-bold">Vest & Spend</h5>
+                        <p>
+                          Like any living system, the reserves underpinning cashless promises undergo a natural birth, growth and decay cycle. If no reciprocity is found, the value of reserves build up over time, rewarding the recipient for holding on to the promise token. As they mature, promises bear fruit in the form of hard currency reserves, that can be picked into the receiver’s wallet as soon as they vest.
+                        </p>
+                      </Col>
+                    </Row>
                   </div>
                 </div>
               </Col>
@@ -93,110 +100,74 @@ function Details() {
               </Col>
             </Row>
           </Container>
-        </div>         
+        </div>
         <div>
           <Container>
-            <Row>
-              <Col md="12">
-                <h2 className="title">Gaurantees</h2>
-                {/*<div className="cashless-cheque-container">
-                  <img
-                    alt="..."
-                    src={require("assets/img/cashless_cheque.png")}
-                  ></img>
-                </div>*/}
-              </Col>
-            </Row>
-            <Row>
-               <Col md="12">
-                  <p className="text-small">
-                    We aim to build a type of credit that cares about the communities it's connected to. But credit has to be <b>reliable</b> to be useful. We provide for multiple layers of gaurantee. Three layers of gaurantee at your discretion.
+            <Card>
+              <CardBody className="p-4 text-center">
+                <Row>
+                  <Col md={6}>
+                    <img alt="rating" src={require("assets/img/clip-rating.png")} height={150} className="m-3"></img>
+                    <h6>Reputation as gaurantee</h6>
+                    <p>
+                      The simplest way to 'back yourself' is by having a good reputation. Cashless promises are visible to anyone in the community and cannot be deleted for 18 months. There's no actual punishment for defaulting on promises, but having a reputation in good standing is a valuable thing in any real community. 
                     </p>
-                </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Card>
-                  <CardBody>
-                      <Row>
-                        <Col md="3">
-                          <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_bro9ocvt.json"  background="transparent"  speed="1"  style={{width: "100%", height: "auto"}}  loop  autoplay></lottie-player>
-                        </Col>
-                        <Col md="9">
-                          <h6>Reputation as gaurantee</h6>
-                          <p className="text-small">
-                          The simplest way to 'back yourself' is by having a good reputation. Cashless promises are visible to anyone in the community and cannot be deleted for 18 months. There's no actual punishment for defaulting on promises, but having a reputation in good standing is a valuable thing in any real community. 
-                          </p>
-                        </Col>
-                      </Row>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Card>
-                  <CardBody>
-                      <Row>
-                        <Col md="3">
-                          <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_eujvgitv.json"  background="transparent"  speed="1"  style={{width: "100%", height: "auto"}} loop  autoplay></lottie-player>
-                          
-                        </Col>
-                        <Col md="9">
-                          <h6>Personal crypto reserves as a gaurantee </h6>
-                          <p className="text-small">
-    If you’re looking to have your credit more widely accepted, you can back yourself by 'staking' stable coins like USDC or DAI. Just transfer reserves into your wallet. We're non-custodial - which means you maintain absolute control of your reserve balance - but it becomes visible in your profile. If you choose, you can even 'lock' in the reserves so that your recipient is 100% gauranteed on a particular credit, once it vests. 
-                          </p>
-                        </Col>
-                      </Row>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Card>
-                  <CardBody>
-                    <Row>
-                      <Col md="3">
-                         <lottie-player src="https://assets4.lottiefiles.com/temp/lf20_DGLtjO.json"  background="transparent"  speed="1" style={{width: "100%", height: "auto"}} hover autoplay loop ></lottie-player>
-                         <br />
-                           <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_rdsqlthv.json"  background="transparent"  speed="1" style={{width: "100%", height: "auto"}}  loop autoplay></lottie-player>
-                      </Col>
-                      <Col md="9">
-                        <h4 className="info-title">Community tokens act as insurance and focal points</h4>
-                        <p className="text-small">
-      Community tokens are the final piece of the puzzle. Anyone can create a community and create a token. For example, a community of ‘tech startups’ could focus on connecting web developers to coders, or a localized community token might focus on a particular local city.
-                        </p>
-                        <p className="text-small">
-      Community nodes act as focal points that increase the likelihood of payments while also providing some degree member validation. They provide a final gaurantee of payments (up to a point) for every member of their community.
-                        </p>
-                        <p className="text-small"> 
-
-      Each community has token holders who decide how much risk they're willing to take and who they'll cover. It is possible to buy or earn community tokens by engaging in successful trades with that community.
-                        </p>
-                        <p className="text-small">
-      Communities nodes also provide places for people to connect and engage with each other, and token holders are incentivised to build their communities. Each community token comes with a token-bonding contract that locks in an extra layer of collective protection for all members - visible to everyone and managed by members of that community.
-                        </p>
-                        <p className="text-small">
-                        </p>
-                      </Col>
-                    </Row>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
-            <Row>
-              <Col md="12">
-                <div className="info info-horizontal">
-                  <div className="description">
-                    <h4 className="info-title">Social and viral </h4>
+                  </Col>
+                  <Col md={6}>
+                    <img alt="rating" src={require("assets/img/clip-payment.png")} height={150}  className="m-3"></img>
+                    <h6>Personal crypto reserves as a gaurantee </h6>
                     <p className="text-small">
+                      If you’re looking to have your credit more widely accepted, you can back yourself by 'staking' stable coins like USDC or DAI. Just transfer reserves into your wallet. We're non-custodial - which means you maintain absolute control of your reserve balance - but it becomes visible in your profile. If you choose, you can even 'lock' in the reserves so that your recipient is 100% gauranteed on a particular credit, once it vests. 
+                    </p>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+
+            <Row className="mt-5">
+              <Col md={4}>
+                <lottie-player src="https://assets6.lottiefiles.com/packages/lf20_dsmgi54l.json" background="transparent" speed="1" style={{width: "100%", height: "auto"}} loop autoplay></lottie-player>
+              </Col>
+              <Col md={8}>
+                <h4 className="info-title">Community tokens act as insurance and focal points</h4>
+                <p>
+                  Community tokens are the final piece of the puzzle. Anyone can create a community and create a token. For example, a community of ‘tech startups’ could focus on connecting web developers to coders, or a localized community token might focus on a particular local city.
+                </p>
+                <p>
+                  Community nodes act as focal points that increase the likelihood of payments while also providing some degree member validation. They provide a final gaurantee of payments (up to a point) for every member of their community.
+                </p>
+              </Col>
+            </Row>
+            <Card className="description">
+              <CardBody>
+                <p>
+                  Each community has token holders who decide how much risk they're willing to take and who they'll cover. It is possible to buy or earn community tokens by engaging in successful trades with that community.
+                </p>
+                <p className="mb-0">
+                  Communities nodes also provide places for people to connect and engage with each other, and token holders are incentivised to build their communities. Each community token comes with a token-bonding contract that locks in an extra layer of collective protection for all members - visible to everyone and managed by members of that community.
+                </p>
+              </CardBody>
+            </Card>
+
+            <Row>
+              <Col md="9">
+                <div>
+                  <div>
+                    <h4 className="info-title">Social and viral </h4>
+                    <p>
                       An obligation can be a hard nosed promise of payment. Or it can just be a nice <b>thank you</b> - a promise to pay it forward that you might attach to a more social expression of gratitude. Gratitude can also be money. Once we recover from our collective PTSD about money, we can hopefully start to see the new possibilities.
                     </p>
                   </div>
                 </div>
               </Col>
+              <Col md="3" className="pt-4">
+                <img
+                  alt="social"
+                  src={require("assets/img/clip-welcome.png")}
+                ></img>
+              </Col>
+            </Row>
+            <Row>
               <Col md="12">
                 <div className="cashless-payit4wd-container">
                   <img
--- src/components/Why.js
+++ src/components/Why.js
@@ -8,56 +8,40 @@ import { Card, Container, Row, Col } from "reactstrap";
 function Why() {
   return (
   <>
-    <div className="features-8 cashless-features section-image"
-      style={{ 
-        backgroundImage: "url(" + require("assets/img/light_house_pink.jpg") + ")"}}>
+    <div className="features-8 cashless-features bg-sunset">
       <Container>
         <Row>
-            <Col className="mr-auto ml-auto text-center" md="8">
-              <h2 className="title">Why?</h2>
-            </Col>
+          <Col md={7} className="pr-5">
+            <h2 className="title">Why?</h2>
+            <div>
+              <h5 className="info-title">Build community</h5>
+              <p>
+               Community organizers have long known that interest free mutual credit is an amazing tool for helping communities grow and become self reliant. But until now, such communities have lacked the extra liquidity and transparency that cashless credit can provide.
+              </p>
+              <img alt="money" src={require("assets/img/graphic-money.svg")} width={60} className="mr-4"></img>
+              <img alt="time" src={require("assets/img/graphic-time.svg")} width={60}></img>
+            </div>
+          </Col>
+          <Col md={5}>
+            <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_fjv8qxqn.json"  background="transparent"  speed="0.6"  style={{width: "100%", height: "auto"}}  loop  autoplay></lottie-player>
+          </Col>
         </Row>
         <Row>
-          <Col className="px-0" md="6">
-            <Col sm="12">
-              <div className="info info-horizontal">
-                <div className="icon">
-                  <i className="now-ui-icons objects_umbrella-13"></i>
-                </div>
-                <div className="description">
-                  <h5 className="info-title">End Poverty</h5>
-                  <p className="description">
-                    Credit unions, micro-credit and community banks are well known to be effective ways to lift communities out of poverty. Cashless leverages recent advances in distributed finance and cryptography to do the same thing without banks, and without interest, in a much more scalable way.
-                  </p>
-                </div>
-              </div>
-              <div className="info info-horizontal">
-                <div className="icon">
-                  <i className="now-ui-icons objects_spaceship"></i>
-                </div>
-                <div className="description">
-                  <h5 className="info-title">Build community</h5>
-                  <p className="description">
-                   Community organizers have long known that interest free mutual credit is an amazing tool for helping communities grow and become self reliant. But until now, such communities have lacked the extra liquidity and transparency that cashless credit can provide.
-                  </p>
-                </div>
-              </div>
-            </Col>
+          <Col md={4}>
+            <img
+              alt="work together"
+              src={require("assets/img/clip-brainstorm.png")}
+            ></img>
           </Col>
-          <Col className="px-0" md="6">
-            <Col sm="12">
-              <div className="info info-horizontal">
-                <div className="icon">
-                  <i className="now-ui-icons objects_key-25"></i>
-                </div>
-                <div className="description">
-                  <h5 className="info-title">No barriers, no bias and discrimination</h5>
-                  <p className="description">
-                   Credit barriers, bias and discrimination plague the financial industry through biased 'credit reports' or even just narrow mindedness that puts banks and other financial gatekeepers in the role of limiting access to resources. Instead of 'removing barriers' the cashless approach is to empower communities to take control of their own credit and exercize their collective power. When you control your own networks of sharing and credit you don't have to ask for permission from anybody.
-                  </p>
-                </div>
+          <Col md={8}>
+            <div>
+              <div>
+                <h5 className="info-title">End Poverty</h5>
+                <p>
+                  Credit unions, micro-credit and community banks are well known to be effective ways to lift communities out of poverty. Cashless leverages recent advances in distributed finance and cryptography to do the same thing without banks, and without interest, in a much more scalable way.
+                </p>
               </div>
-            </Col>
+            </div>
           </Col>
         </Row>
       </Container>
