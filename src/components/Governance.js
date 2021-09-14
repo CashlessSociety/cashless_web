1de1f03c1bd75061bef58efbb9e5cc1c72889197
Karen Lo <karen@laterolabs.com>
2021-09-13 17:34:43 -0700

    Scss cleanup and new graphic for "This is the beginning" and "Governance" sections.

--- src/components/Governance.js
+++ src/components/Governance.js
@@ -7,51 +7,41 @@ import { Card, Container, Row, Col } from "reactstrap";
 
 function Governance() {
   return (
-    <div className="features-8 govern section-image"
-      style={{
-        backgroundImage: "url(" + require("assets/img/bg22.jpg") + ")",
-      }}>
+    <div className="features-8 govern">
       <Col className="mr-auto ml-auto text-center" md="8">
-        <h2 className="title">Governance
-</h2>
-        <h4 className="description">
-          <p className="comment dark">Cashless is a Distributed Autonomous Organisation</p>
+        <h2 className="title">
+          Governance
+        </h2>
+        <h4 className="lead">
+          Cashless is a Distributed Autonomous Organisation
         </h4>
       </Col>
-      <Container fluid>
+      <Container>
         <Row>
           <Col className="px-0" md="6">
             <Col sm="12">
-              <div className="info info-horizontal">
-                <div className="icon">
-                  <i className="now-ui-icons design-2_html5"></i>
-                </div>
+              <div className="info">
                 <div className="description">
                   <h5 className="info-title">DAO to differ</h5>
-                   <p className="description">Join Cashless and you're not joining a company, you're joining a community of like minded individuals building software and communities that matter.</p>
+                   <p>Join Cashless and you're not joining a company, you're joining a community of like minded individuals building software and communities that matter.</p>
                 </div>
               </div>
-              <div className="info info-horizontal">
-                <div className="icon">
-                  <i className="now-ui-icons design_palette"></i>
-                </div>
+              <div className="info">
+
                 <div className="description">
                   <h5 className="info-title">Committed to Open Source</h5>
-                  <p className="description">
+                  <p>
                     Cashless is an open source MPL licensed protocol with a truly open source reference implementation. There are no 'hooks' or embedded crypto tokens required to run it. We want anyone and everyone to be able to build on this protocol. 
                   </p>
                 </div>
               </div>
- <div className="info info-horizontal">
-                <div className="icon">
-                  <i className="now-ui-icons design_palette"></i>
-                </div>
+              <div className="info">
                 <div className="description">
                   <h5 className="info-title">Business model</h5>
-                  <p className="description">
+                  <p>
                     Cashless hopes to create a million communities that build on top of the open protocol. Their success directly influences our success and vice versa. Cashless will make early investments (and eventually automated investments) in the communities that issue community tokens on top of Cashless protocol, using Cashless software.
                     </p>
-                    <p className="description">Cashless will be building an AI recommendation business, which will essentially act as a concierge to facilitate credit worthy  transactions. It won't be necessary to use this particular app, but it will be an option amongst many that allows you to interact with the protocol in a way optimized for your success.
+                    <p>Cashless will be building an AI recommendation business, which will essentially act as a concierge to facilitate credit worthy  transactions. It won't be necessary to use this particular app, but it will be an option amongst many that allows you to interact with the protocol in a way optimized for your success.
                   </p>
                 </div>
               </div>
@@ -59,35 +49,26 @@ function Governance() {
           </Col>
           <Col className="px-0" md="6">
             <Col sm="12">
-              <div className="info info-horizontal">
-                <div className="icon">
-                  <i className="now-ui-icons design-2_ruler-pencil"></i>
-                </div>
+              <div className="info">
                 <div className="description">
                   <h5 className="info-title">The Cashless DAO</h5>
-                  <p className="description">
+                  <p>
                     Cashless is an organisation of people building software and communities that run on top of that protocol. You don't have to be part of our organisation to contribute to the protocol, but if you want to become a member of the DAO you can earn governance tokens that will give you voting rights in the DAO and a share of any profits generated down the line. 
                   </p>
                 </div>
               </div>
-              <div className="info info-horizontal">
-                <div className="icon">
-                  <i className="now-ui-icons design-2_ruler-pencil"></i>
-                </div>
+              <div className="info">
                 <div className="description">
                   <h5 className="info-title">Coin offering</h5>
-                  <p className="description">
+                  <p>
                     Governance tokens are not currently available for sale, but there will eventually be a coin offering that will also allow investors to take part in our success. 
                   </p>
                 </div>
               </div>
-              <div className="info info-horizontal">
-                <div className="icon">
-                  <i className="now-ui-icons design-2_ruler-pencil"></i>
-                </div>
+              <div className="info">
                 <div className="description">
                   <h5 className="info-title">Current status</h5>
-                  <p className="description">
+                  <p>
                     Right now we are just a group of people building apps that matter. We are still evaluating the best ways to establish our DAO. This is a decision that needs to be made carefully. Once we launch the DAO, anyone with an interest in becoming a part of our innovation community will be able to sign the agreement, become a member and start earning (or investing in) governance tokens. 
                   </p>
                 </div>
--- src/components/Why.js
+++ src/components/Why.js
@@ -14,8 +14,8 @@ function Why() {
           <Col md={7} className="pr-5">
             <h2 className="title">Why?</h2>
             <div>
-              <h5 className="info-title">Build community</h5>
-              <p>
+              <h3 className="info-title">Build community</h3>
+              <p className="lead">
                Community organizers have long known that interest free mutual credit is an amazing tool for helping communities grow and become self reliant. But until now, such communities have lacked the extra liquidity and transparency that cashless credit can provide.
               </p>
               <img alt="money" src={require("assets/img/graphic-money.svg")} width={60} className="mr-4"></img>
@@ -36,8 +36,8 @@ function Why() {
           <Col md={8}>
             <div>
               <div>
-                <h5 className="info-title">End Poverty</h5>
-                <p>
+                <h3 className="info-title">End Poverty</h3>
+                <p className="lead">
                   Credit unions, micro-credit and community banks are well known to be effective ways to lift communities out of poverty. Cashless leverages recent advances in distributed finance and cryptography to do the same thing without banks, and without interest, in a much more scalable way.
                 </p>
               </div>
