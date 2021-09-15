03542a94be94f23dd6d3cd1871e0ed580dca6a97
Karen Lo <karen@laterolabs.com>
2021-09-14 17:36:57 -0700

    Updating graphics/visuals

--- src/components/CurrentStatus.js
+++ src/components/CurrentStatus.js
@@ -12,33 +12,30 @@ function CurrentStatus() {
     <>
       <div id="status" className="cashless-features current-status">
         <Container>
-        <h2 className="title">Current Status</h2>
+        
          <Row>
-            <Col md={6}>
-              <div>
-                <div>
-                  <p>
-                  Right now we have:
-                  <ul><li>A small team</li>
-                      <li>A proof of concept <a href="https://startups.cashless.social">(deployed on main net)</a></li>
-                      <li>A vision of where we're going</li>
-                  </ul>
-                  </p>
-                  <p>
-                    We're working on:
-                    <ul><li><a href="https://github.com/CashlessSociety/cashless-ui">The 100% open source MVP and reference implementation</a></li>
-                    <li>A comprehensive white paper.</li>
-                    <li>Working out how to governance our DAO.</li>
-                    <li>Finding our founding  team</li>
-                    </ul>
-                  </p>
-                </div>
-              </div>
+            <Col md={8}>
+              <h2 className="title">Current Status</h2>
+              <p>
+              Right now we have:
+              <ul><li>A small team</li>
+                  <li>A proof of concept <a href="https://startups.cashless.social">(deployed on main net)</a></li>
+                  <li>A vision of where we're going</li>
+              </ul>
+              </p>
+              <p>
+                We're working on:
+                <ul><li><a href="https://github.com/CashlessSociety/cashless-ui">The 100% open source MVP and reference implementation</a></li>
+                <li>A comprehensive white paper.</li>
+                <li>Working out how to governance our DAO.</li>
+                <li>Finding our founding  team</li>
+                </ul>
+              </p>
             </Col>
-            <Col md={6}>
+            <Col md={4} className="pt-5">
               <img
-                alt="social"
-                src={require("assets/img/clip-coop.png")}
+                alt="working"
+                src={require("assets/img/working.jpg")}
               ></img>
             </Col>
           </Row>
