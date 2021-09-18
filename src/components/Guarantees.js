1a4baf7cb9c012765f1977485cefc40a2769e3ec
Miles Thompson <utunga@gmail.com>
2021-09-18 12:50:39 +1200

    Some content tweaks (#22)

--- src/components/Guarantees.js
+++ src/components/Guarantees.js
@@ -7,10 +7,10 @@ import { Card, CardBody, Container, Row, Col } from "reactstrap";
 function Guarantees() {
   return (
     <>
-      <div className="features-5">
+      <div className="features-5" id="gaurantees">
         <div>
           <Container>
-            <h4 className="info-title">The three guarantees</h4>
+            <h2 className="title">The three guarantees</h2>
             <Row>
               <Col md={6}>
                 <Card>
@@ -18,7 +18,7 @@ function Guarantees() {
                     <img alt="rating" src={require("assets/img/clip-rating.png")} height={150} className="m-3"></img>
                     <h6>Your reputation</h6>
                     <p>
-                    Cashless promises are visible to anyone in the community, signed by you, and cannot be deleted for 18 months. Credit in good standing means paying your debts.
+                    Cashless promises are visible to anyone in the community, signed by you, and cannot be deleted for 18 months. <b>Credit in good standing</b> counts for a lot. That means doing good work, paying your debts and generally building your reputation.
                     </p>
                   </CardBody>
                 </Card>
@@ -29,7 +29,7 @@ function Guarantees() {
                     <img alt="rating" src={require("assets/img/clip-payment.png")} height={150}  className="m-3"></img>
                     <h6>Your personal reserves </h6>
                     <p className="text-small">
-                      To have your credit more widely accepted, back yourself by 'staking' stable coins like USDC or DAI. Reserves placed in our non-custodial contract are transparent to all. 
+                      To have your credit more widely accepted, <b>back yourself by 'staking'</b> any crypto - stable coins like USDC or DAI. Reserves placed in our non-custodial smart contract are transparent to all and can be claimed by your counter-parties as soon as they vest.
                     </p>
                   </CardBody>
                 </Card>
@@ -49,13 +49,13 @@ function Guarantees() {
                         </Col>
                         <Col md={6}>
                         <lottie-player src="https://assets3.lottiefiles.com/temp/lf20_pO3t5Q.json"  background="transparent"  speed="0.6"  style={{width: "100%", height: "200px", marginTop: "-50px", marginBottom: "-30px"}}  loop  autoplay></lottie-player>
-                          <h6 className="text-center">Your community</h6>
+                          <h6 className="text-center">Your Community, Your Community Token</h6>
                           <p>
-                            Communities and community tokens are the final piece of the puzzle.  Community tokens provide a final guarantee of payments (up to a point) for every member of the community using that token.
+                            Communities and <b>community tokens</b> are the final piece of the puzzle.  Community tokens provide a final guarantee of payments (up to an observable limit) for every member of that community using that token.
                      
                           </p>
                           <p className="mb-0">
-                            Communities also act as focal points to increase the likelihood of reciprocal payment provide places for people to connect and engage with each other. Members / token holders are incentivised to build their communities. Each community token comes with a token-bonding contract that locks in an extra layer of collective protection for all members - visible to everyone and managed by members of that community.
+                            Communities act as focal points to increase the likelihood of reciprocal payment and provide places for people to connect and engage with each other. This can be done on any social platform. Members / token holders are incentivised to build their communities. Each community token comes with a token-bonding contract that locks in an extra layer of <b>collective reserves</b> protection for all members - visible to everyone and managed by members of that community.
                           </p>
                           
                           
@@ -64,9 +64,6 @@ function Guarantees() {
                           </p>
                           <p>
                             Each community has token holders who decide how much risk they're willing to take and who they'll cover. It is possible to buy or earn community tokens by engaging in successful trades with that community.
-                          </p>
-                          <p className="mb-0">
-                            Communities nodes also provide places for people to connect and engage with each other, and token holders are incentivised to build their communities. Each community token comes with a token-bonding contract that locks in an extra layer of collective protection for all members - visible to everyone and managed by members of that community.
                           </p>*/}
                         </Col>
                       </Row>
